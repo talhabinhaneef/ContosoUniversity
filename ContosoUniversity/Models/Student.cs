1c7a782941336fa7d859a70cd1c61748197e3739
talhabinhaneef <talhahaneef560@gmail.com>
2023-02-28 05:22:33 +0500

    error

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 namespace ContosoUniversity.Models
 {
-    public class Student : Person
+    public class Student
     {
         public int ID { get; set; }
         [Required]
