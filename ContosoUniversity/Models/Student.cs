0e476f7d3f6609ded4ef81050224030ba3b5d5e2
talhabinhaneef <talhahaneef560@gmail.com>
2023-02-28 05:22:49 +0500

    Revert "error"
    This reverts commit 1c7a782941336fa7d859a70cd1c61748197e3739.

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 namespace ContosoUniversity.Models
 {
-    public class Student
+    public class Student : Person
     {
         public int ID { get; set; }
         [Required]
