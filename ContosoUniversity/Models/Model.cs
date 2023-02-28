5f3351258f0a851f23dee0203e40ac992751243c
talhabinhaneef <talhahaneef560@gmail.com>
2023-02-28 05:13:39 +0500

    Added Followings
    Mapped inheritance to database
    Created the Person class
    Updated Instructor and Student
    Added Person to the model
    Created and update migrations
    Tested the implementation

--- ContosoUniversity/Models/Model.cs
+++ ContosoUniversity/Models/Model.cs
@@ -1,26 +1,22 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System;
-using System.Collections.Generic;
 
 namespace ContosoUniversity.Models
 {
-    public class Student : Person
+    public abstract class Person
     {
         public int ID { get; set; }
+
         [Required]
-        [StringLength(50, MinimumLength = 4)]
+        [StringLength(50)]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
         [Required]
-        [StringLength(50, MinimumLength = 4)]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
         [Column("FirstName")]
         [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        [Display(Name = "Enrollment Date")]
-        public DateTime EnrollmentDate { get; set; }
+
         [Display(Name = "Full Name")]
         public string FullName
         {
@@ -29,7 +25,5 @@ namespace ContosoUniversity.Models
                 return LastName + ", " + FirstMidName;
             }
         }
-
-        public ICollection<Enrollment> Enrollments { get; set; }
     }
 }
